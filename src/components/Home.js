e9b4f42060d453d4124ddb4a08b96cb8a9019326
Deneth  Godakandagei <138301396+DenethGodakandagei@users.noreply.github.com>
2023-07-21 17:24:56 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,32 +11,50 @@ const Home = () => {
           className="col-12 min-vh-100"
           style={{ paddingTop: " 180px" }}
         >
+           <div className="img_main container mt-sm-3 animation 	.d-none d-sm-none">             
+              <img src={Main} />
+            </div>
           <div className="d-sm-flex align-items-center justify-content-between container">
             <div className="col-md-6 mx-auto mb-4 mb-sm-0 headline">
-              <h1 className="display-4 my-4 font-weight-bold container animation">
+              <h1 className="display-4 my-4 font-weight-bold container animation d-none d-sm-block">
                 <span style={{ color: "#6703e0" }}>
                   Hey, <br />I Am Software
                   <div className="animate_name">Developer...</div>
                 </span>
               </h1>
+              <h2 className="display-4 my-4 font-weight-bold container animation 	.d-none d-sm-none">
+                <span style={{ color: "#6703e0" }}>
+                  Hey, <br />I Am Software
+                  <div className="animate_name">Developer...</div>
+                </span>
+              </h2>
               <p className="hero_decription container mt-sm-4">
                 I can help you solve a problem, build a product or grow existing
                 project.
               </p>
+              <div className="d-none d-sm-block .d-md-none .d-lg-block">
               <a className="btn Email" href="#contact">
                 Email me
               </a>
               <a href={pdfFile} download="cv.pdf" className=" btn btn_Cv">
                 Download CV
               </a>
+              </div>
+              <div className="flex .d-none d-sm-none">
+              <a className="btn Email mx-3 m-3" href="#contact">
+                Email me
+              </a>
+              <a href={pdfFile} download="cv.pdf" className=" btn btn_Cv mx-3 " style={{fontSize:'20px'}}>
+                Download CV
+              </a>
+              </div>
+
             </div>
-            <div className="img_main container mt-sm-3 animation">
-              <div className="white__gradient " />
+            <div className="img_main container mt-sm-3 animation d-none d-sm-block .d-md-none .d-lg-block">             
               <img src={Main} />
             </div>
           </div>
         </div>
-        <div className="space" style={{ height: "80px" }}></div>
       </section>
     </div>
   );
